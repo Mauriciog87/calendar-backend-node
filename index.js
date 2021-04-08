b89346b4bdc1cc09c8b13c874535cf6c6ea90f38
Mauricio <mauriciog87@gmail.com>
2021-04-07 21:17:30 -0300

    envirioment variables and public folder

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 
+require('dotenv').config();
+
 const app = express();
 
-app.get('/', (request, response) => {
-    response.json({
-        ok: true
-    });
-});
+// app.get('/', (request, response) => {
+//     response.json({
+//         ok: true
+//     });
+// });
 
-app.listen(3001, () => {
-    console.log(`Server running on port: ${4000}`);
+app.use(express.static('public'));
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port: ${process.env.PORT}`);
 });
