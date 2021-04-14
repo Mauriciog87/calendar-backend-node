5f0e168f04e6b2e3608f2aab1b2539c4cab2030b
Mauricio <mauriciog87@gmail.com>
2021-04-13 23:57:40 -0300

    status codes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,32 @@
 const express = require('express');
 
 const createUser = (request, response = express.response) => {
+    const {name, email, password} = request.body;
+
+    if (name.length < 5) {
+        return response.status(400).json({
+            ok: false,
+            msg: 'Name must be 5 character lentgh at least'
+        });
+    }
+
     response.json({
         ok: true,
-        msg: 'register'
+        msg: 'register',
+        name,
+        email,
+        password
     });
 }
 
 const login  = (request, response = express.response) => {
+    const {email, password} = request.body;
+
     response.json({
         ok: true,
-        meg: 'login'
+        meg: 'login',
+        email,
+        password
     });
 }
 
