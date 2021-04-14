e61ddddd44fe3e3df8cff1575a496bc31163a3de
Mauricio <mauriciog87@gmail.com>
2021-04-13 23:50:37 -0300

    accept json bro

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ require('dotenv').config();
 
 const app = express();
 
+// read and parse body
+app.use(express.json());
+
 //Routes
 app.use('/api/auth', require('./routes/auth'));
 
 //Public folder
 app.use(express.static('public'));
 
+//listen request
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port: ${process.env.PORT}`);
 });
